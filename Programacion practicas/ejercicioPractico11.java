bb47cfd7e7edf5a11749a063bc9f328dae36fd37
Chantal <chda2022@gmail.com>
2023-02-24 17:27:29 +0100

    nuevos ejercicios de practica

---
+++ Programacion practicas/ejercicioPractico11.java
@@ -0,0 +1,16 @@
+public class ejercicioPractico11 {
+    public static void main(String[] args) {
+        
+        int ancho = 30;
+        int largo = 15;
+        System.out.println("El área del rectángulo es: " + areaRectangulo(ancho, largo));
+    }
+    public static int areaRectangulo(int width, int large){
+        width = 30;
+        large = 15;
+        
+        int resultado = width * large;
+
+        return resultado;
+    }
+}
