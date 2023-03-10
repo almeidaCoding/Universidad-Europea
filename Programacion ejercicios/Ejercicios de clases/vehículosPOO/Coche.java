0d9a3d440e27137428717b059e66b6547ae255b1
Chantal <chda2022@gmail.com>
2023-03-10 17:10:08 +0100

    Nuevos ejercicios POO

---
+++ Programacion ejercicios/Ejercicios de clases/vehículosPOO/Coche.java
@@ -0,0 +1,10 @@
+public class Coche extends Vehiculo {
+    private int cilindrada; 
+    public Coche(int c) { 
+        super();
+        this.cilindrada = c; 
+    }
+    public void quemaRueda() { 
+        System.out.println("Fffshhhhhhhhhhh");
+    }
+}
