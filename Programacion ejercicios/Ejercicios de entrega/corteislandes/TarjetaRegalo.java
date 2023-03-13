4a715f176effe9326dfc2466421c862c21ce1602
Chantal <chda2022@gmail.com>
2023-03-14 00:57:47 +0100

    cambios

--- Programacion ejercicios/Ejercicios de entrega/corteislandes/TarjetaRegalo.java
+++ Programacion ejercicios/Ejercicios de entrega/corteislandes/TarjetaRegalo.java
@@ -1,8 +1,7 @@
 package corteislandes;
 import java.util.Random;
 
-// La empresa El Corte Islandés nos ha encargado una aplicación para gestionar las tarjetas regalo. Como primer paso para implementar la aplicación, es necesario crear la clase principal. Implementa la clase TarjetaRegalo. Cuando se crea una nueva tarjeta, se le da un saldo y se asigna de forma automática un número de 5 dígitos. Si se intenta gastar más dinero del que tiene la tarjeta, se debe mostrar un mensaje de error. Dos tarjetas regalo se pueden fusionar creando una nueva tarjeta con la suma del saldo que tenga cada una y un nuevo número aleatorio de 5 cifras. Al fusionar dos tarjetas en una, las dos tarjetas originales se quedarían con 0 € de saldo.
-
+//Clase TarjetaRegalo con sus métodos y constructor
 public class TarjetaRegalo {
 
     private double saldo;
@@ -17,8 +16,6 @@ public class TarjetaRegalo {
         numCard = numCardAsignado;
         numCardAsignado++;
     }
-
-    //método getter
     public int getNumAleatorio(){
         Random random = new Random();
         numeroAleatorio = random.nextInt(90000) + 10000;
